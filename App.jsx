747cb81d5dc62acd25213e2dac00166a2202e206
Deepiga <126891628+Deepikagowtham@users.noreply.github.com>
2024-05-30 18:37:54 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header.jsx';
+import MainContent from './MainContent.jsx';
+import Footer from './Footer.jsx';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
