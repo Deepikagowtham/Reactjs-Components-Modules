7d10b8670829878b782ca0a72e253dfc6f342958
Deepiga <126891628+Deepikagowtham@users.noreply.github.com>
2024-05-31 11:26:42 +0530

    Footer.jsx

--- Footer.jsx
+++ Footer.jsx
@@ -1,11 +1,1 @@
-import React from 'react';
 
-const Footer = () => {
-  return (
-    <footer>
-      <p>&copy; 2024 Fantasy Amusement Park. All rights reserved.</p>
-    </footer>
-  );
-};
-
-export default Footer;
