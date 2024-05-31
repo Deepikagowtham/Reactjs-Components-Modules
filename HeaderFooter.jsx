13fda148c50423239272087eee68d86c85ac189a
Deepiga <126891628+Deepikagowtham@users.noreply.github.com>
2024-05-31 11:25:19 +0530

    Add files via upload

---
+++ HeaderFooter.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <h1>Welcome to Fantasy Amusement Park</h1>
+  </header>
+);
+
+const Footer = () => (
+  <footer>
+    <p>&copy; 2024 Fantasy Amusement Park. All Rights Reserved.</p>
+  </footer>
+);
+
+export { Header, Footer };
